f51af58cf4841e4c90bf50a539f5f77e9d906516
OGaye <gayeomar@hotmail.com>
2023-06-05 08:21:47 -0400

    Separate config

--- src/main/java/com/bofa/demo/faces/config/DemoJsfMigrationApplicationConfig.java
+++ src/main/java/com/bofa/demo/faces/config/DemoJsfMigrationApplicationConfig.java
@@ -6,24 +6,19 @@ import jakarta.faces.webapp.FacesServlet;
 import jakarta.servlet.ServletContext;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.context.ServletContextAware;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class DemoJsfMigrationApplicationConfig { //implements ServletContextAware{
 
-@SpringBootApplication
-public class DemoJsfMigrationApplication { //implements ServletContextAware{
-    public static void main(String[] args) {
-        SpringApplication.run(DemoJsfMigrationApplication.class, args);
-    }
-
-/*    @Bean
+    @Bean
     public static CustomScopeConfigurer viewScope() {
         CustomScopeConfigurer configurer = new CustomScopeConfigurer();
         configurer.setScopes(new ImmutableMap.Builder<String, Object>().put("view", new ViewScope()).build());
         return configurer;
-    }*/
+    }
 /*    @Bean
     public ServletRegistrationBean<FacesServlet> facesServletBean() {
         var facesServlet = new FacesServlet();
@@ -34,7 +29,7 @@ public class DemoJsfMigrationApplication { //implements ServletContextAware{
         return bean;
     }*/
 
-    @Bean
+/*    @Bean
     public ServletRegistrationBean<FacesServlet> facesServletBean(ServletContext servletContext) {
         var facesServlet = new FacesServlet();
         var bean = new ServletRegistrationBean<FacesServlet>();
@@ -43,7 +38,7 @@ public class DemoJsfMigrationApplication { //implements ServletContextAware{
         bean.setLoadOnStartup(1);
         servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
         return bean;
-    }
+    }*/
 
 /*
     @Override
